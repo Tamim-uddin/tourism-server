58573798758975e8fe00e906bbe6ac96fab1ff94
Tamim-uddin <muhammadtamimuddin@gmail.com>
2021-10-30 02:04:35 +0600

    port-added

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middlewar
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ww2yo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// console.log(uri);
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('tour-wb');
+        const tourCollection = database.collection('tours');
+
+        // post api
+        app.post('/tours', async (req, res) => {
+            const tour = req.body;
+            console.log('hiting the post api', tour);
+            const result = await tourCollection.insertOne(tour);
+            console.log(result);
+            res.json(result)
+
+        });
+
+
+
+
+        // get api
+        app.get('/tours', async (req, res) => {
+            const cursor = tourCollection.find({});
+            const tour = await cursor.toArray();
+            res.send(tour);
+
+        });
+        
+    }
+    finally {
+        // await client.close();
+    }
+
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('tourism website')
+});
+
+app.listen(port, () => {
+    console.log('torism website server', port)
+})
